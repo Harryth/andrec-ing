72626f3b829bf1c9aad9fa9b23c29ef08ac746ed
Your Name <you@example.com>
2019-02-25 10:24:19 -0500

    Database log in information

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+    /*$db_hostname = "mysql17.000webhost.com";
+	$db_database = "a9851968_wlsb";
+	$db_username = "a9851968_root";
+	$db_password = "rM04_Tl05";*/
+	
+	$db_hostname = "localhost";
+	$db_database = "andrec-ing";
+	$db_username = "root";
+	$db_password = "";
+?>

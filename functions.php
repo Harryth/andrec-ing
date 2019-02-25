4ff69c5afad4a88aebf0f4b4888d23d62d963b2e
Your Name <you@example.com>
2019-02-25 10:04:02 -0500

    Unifying functions in one file

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+    function sanitizeString($mysqli,$var)
+	{
+		$var = strip_tags($var);
+		$var = htmlentities($var);
+		$var = stripslashes($var);
+		return $mysqli->real_escape_string($var);
+	}
+
+    function permissions($user,$page_perm,$mysqli){
+		$mysqli -> query("SET NAMES 'utf8'");
+		$result = $mysqli -> query("SELECT `permissions` FROM users WHERE user = '$user';");
+		$row = $result -> fetch_array(MYSQLI_BOTH);
+		$user_perm = $row['permissions'];
+		
+		return ($user_perm & $page_perm) == $page_perm;
+	}
+?>
